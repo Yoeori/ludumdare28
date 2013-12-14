14c272b69eba203a38967d60b9725a2fd01ff890
Yoeori <yoeori@live.nl>
2013-12-14 18:15:41 +0100

    Migrated timer to his own class

---
+++ src/js/Timer.js
@@ -0,0 +1,44 @@
+var Timer = Class.extend({
+	
+	time : 0,
+	lastupdate: 1000,
+	isPaused : false,
+	
+	init : function(time) {
+		this.time = time;
+	},
+	
+	update : function(delta) {
+		if(!this.isPaused) {
+			this.lastupdate -= delta;
+			if(this.lastupdate <= 0) {
+				this.lastupdate += 1000;
+				this.time--;
+				if(this.time <= 0) {
+					this.onTimerfinish();
+				}
+			}
+		}
+	},
+	
+	render : function() {
+		var hour = Math.floor((this.time/60));
+		if(hour < 10)
+			hour = "0"+hour;
+		var min = this.time%60;
+		if(min < 10)
+			min = "0"+min;
+		
+		ctx.font="36px victor";
+		ctx.fillText(hour+":"+min,canvas.width-130,50);
+	},
+	
+	addTime : function(timetoadd) {
+		this.time += timetoadd;
+	},
+	
+	onTimerfinish : function() {
+		//World destruction or something
+	}
+	
+});
